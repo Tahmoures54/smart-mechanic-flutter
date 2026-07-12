95a6085ca403316e3c62fb4d59ab2851ebb25d98
Tahmoures54 <37265001+Tahmoures54@users.noreply.github.com>
2026-07-12 22:43:56 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
@@ -25,24 +24,18 @@ class SmartMechanicApp extends StatelessWidget {
     return MaterialApp(
       title: 'مکانیک هوشمند',
       debugShowCheckedModeBanner: false,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [Locale('fa', 'IR')],
       theme: ThemeData(
         useMaterial3: true,
         brightness: Brightness.dark,
         primaryColor: Colors.orange,
         scaffoldBackgroundColor: Colors.black,
-        textTheme: GoogleFonts.vazirmatnTextTheme(Theme.of(context).textTheme.apply(
-          bodyColor: Colors.white,
-          displayColor: Colors.white,
-        )),
         colorScheme: ColorScheme.dark(
           primary: Colors.orange,
           secondary: Colors.amber,
+        ),
+        textTheme: GoogleFonts.vazirmatnTextTheme().apply(
+          bodyColor: Colors.white,
+          displayColor: Colors.white,
         ),
       ),
       home: const HomeScreen(),
