08a021a58856057c8764115ad3e3bcf4c71c5ed5
Tahmoures54 <37265001+Tahmoures54@users.noreply.github.com>
2026-07-13 17:04:59 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
+import 'providers/diagnostic_provider.dart';
+import 'providers/locale_provider.dart';
+import 'services/audio_service.dart';
+import 'services/ai_diagnostic_service.dart';
+import 'services/map_service.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // راه‌اندازی Hive
+  await Hive.initFlutter();
+  await Hive.openBox('diagnostics');
+  await Hive.openBox('history');
+
+  // راه‌اندازی سرویس صوتی
+  final audioService = AudioService();
+  await audioService.init();
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()..checkAuthStatus()),
+        ChangeNotifierProvider(create: (_) => LocaleProvider(LocaleProvider.getDeviceLocale())),
+        ChangeNotifierProvider(create: (_) => DiagnosticProvider()),
+        Provider<AudioService>.value(value: audioService),
+        Provider<AIDiagnosticService>(create: (_) => AIDiagnosticService()),
+        // TODO: کنترلر واقعی نقشه را هنگام در دسترس بودن تزریق کنید
+        Provider<MapService>(create: (_) => MapService(/* GoogleMapController? */)),
       ],
       child: const SmartMechanicApp(),
     ),
@@ -21,8 +44,15 @@ class SmartMechanicApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final localeProvider = Provider.of<LocaleProvider>(context);
     return MaterialApp(
       title: 'مکانیک هوشمند',
+      locale: localeProvider.locale,
+      supportedLocales: LocaleProvider.supportedLocales,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
